8925b7d8753f7d239a78fd739928c3285c06b1a0
hank_Huang <2389813968@qq.com>
2021-03-11 18:34:57 +0800

    hello world 项目第二次提交

--- javaTest/src/HelloWorld.java
+++ javaTest/src/HelloWorld.java
@@ -3,6 +3,8 @@ public class HelloWorld {
         System.out.println("hello world!");
         System.out.println("hello world!");
         System.out.println("hello world!");
+        System.out.println("hello world2!");
+        System.out.println("hello world2!");
 
     }
 }
